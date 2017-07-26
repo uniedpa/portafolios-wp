4dd9533ba114dd51343138b8d32ad1ba3360cfc9
Omar Vasquez <omar@vasquezlima.com>
2017-07-25 23:37:45 -0400

    Added configurations for Multisite

--- .openshift/config/wp-config.php
+++ .openshift/config/wp-config.php
@@ -115,6 +115,12 @@ require_once(getenv('OPENSHIFT_REPO_DIR') . '.openshift/openshift.inc');
 
 /* Multisite */
 define('WP_ALLOW_MULTISITE',true);
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'portafolios-uniedpa2.rhcloud.com');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 
 /*
 |--------------------------------------------------------------------------
